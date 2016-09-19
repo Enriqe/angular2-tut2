8b06db4737fbaafb69c3023be6f12c2a117130a8
Enrique Marroquin <enrique.marr@gmail.com>
2016-09-19 17:39:24 -0500

    init commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>{{title}}</h1> <h2>{{hero.name}}</h2>'
 })
-export class AppComponent { }
+export class AppComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  title = 'Tour of Heroes';
+  // hero = 'Windstorm';
+}
